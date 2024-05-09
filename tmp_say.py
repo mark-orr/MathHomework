25b35bc0d9f81e793774dadfe08eb2c4776f4833
Mark Orr <mo6xj@virginia.edu>
2024-05-09 17:06:41 -0400

    std

--- tmp_say.py
+++ tmp_say.py
@@ -2,7 +2,7 @@ import pyttsx3
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-for i in range(3,10):
+for i in range(0,1):
     engine.setProperty('voice', voices[i].id)
     engine.say("Would You Like To Play A Game?")
     print(i,'\n')
